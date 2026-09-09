3fd1689e53a5bc7bda87c1656fecee198577300f
chitrala varun <varunchitralaa@gmail.com>
2026-09-09 14:08:53 +0530

    refactor: mount structured header in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
-import Nav from './components/Nav'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Nav />
+      <Header />
     </>
   )
 }
