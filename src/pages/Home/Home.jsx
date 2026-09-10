e79f1fdbf55653f3b54918860dd1a52678ec173b
chitrala varun <varunchitralaa@gmail.com>
2026-09-10 12:27:22 +0530

    Create routed Home page composition

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import Solutions from '../../components/Solutions/Solutions'
+import Testimonials from '../../components/Testimonials/Testimonials'
+import Footer from '../../components/Footer/Footer'
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Solutions />
+        <Testimonials />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
