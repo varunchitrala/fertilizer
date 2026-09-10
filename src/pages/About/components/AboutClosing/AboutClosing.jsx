1b8afccb042c12b0995e0ff9183f8d0366a59ada
chitrala varun <varunchitralaa@gmail.com>
2026-09-10 14:05:17 +0530

    refactor: scope About closing stylesheet

--- src/pages/About/components/AboutClosing/AboutClosing.jsx
+++ src/pages/About/components/AboutClosing/AboutClosing.jsx
@@ -1,20 +1,15 @@
+import "./AboutClosing.css";
 import ScrollReveal from "../../../../components/ScrollReveal";
 
 function AboutClosing() {
   return (
     <section className="about-closing">
-      <div className="header-container">
-        <ScrollReveal distance="up">
-          <span className="about-eyebrow about-eyebrow-dark">
-            <i /> Looking ahead
-          </span>
-          <h2>
-            Better inputs.
-            <br />
-            <em>Better farming.</em>
-          </h2>
+      <div className="container">
+        <ScrollReveal distance={40}>
+          <span className="about-eyebrow"><i /> Keep growing</span>
+          <h2>Better inputs. <em>Better farming.</em></h2>
           <a href="mailto:agripexpestiside1995@gmail.com">
-            Start a conversation <span>↗</span>
+            Talk to Agripex <span>↗</span>
           </a>
         </ScrollReveal>
       </div>
