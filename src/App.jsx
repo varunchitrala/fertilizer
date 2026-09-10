62e0ef52208168ad37ccc4c7c8590f422297febd
chitrala varun <varunchitralaa@gmail.com>
2026-09-10 12:29:17 +0530

    Set up React Router page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import Solutions from './components/Solutions/Solutions'
-import Testimonials from './components/Testimonials/Testimonials'
-import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
 import About from './pages/About/About'
 
-function HomePage() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <Solutions />
-        <Testimonials />
-      </main>
-      <Footer />
-    </>
-  )
-}
-
-function AboutPage() {
-  return (
-    <>
-      <Header />
-      <About />
-      <Footer />
-    </>
-  )
-}
-
 function App() {
-  const isAboutPage = window.location.pathname.replace(/\/$/, '') === '/about'
-
-  return isAboutPage ? <AboutPage /> : <HomePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
