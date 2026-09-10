496127c51f1e90d84a9fcc8ac60bf19d14cf9840
chitrala varun <varunchitralaa@gmail.com>
2026-09-10 15:39:16 +0530

    Add products route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Products from './pages/Products/Products'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   )
