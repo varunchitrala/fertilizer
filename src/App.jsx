1d05ba5b1809384bdf3bb3dd2ac498a20f8d8081
chitrala varun <varunchitralaa@gmail.com>
2026-09-10 11:53:17 +0530

    Add About page route

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import Hero from './components/Hero/Hero'
 import Solutions from './components/Solutions/Solutions'
 import Testimonials from './components/Testimonials/Testimonials'
 import Footer from './components/Footer/Footer'
+import About from './pages/About/About'
 
-function App() {
+function HomePage() {
   return (
     <>
       <Header />
@@ -17,6 +18,22 @@ function App() {
       <Footer />
     </>
   )
+}
+
+function AboutPage() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Footer />
+    </>
+  )
+}
+
+function App() {
+  const isAboutPage = window.location.pathname.replace(/\/$/, '') === '/about'
+
+  return isAboutPage ? <AboutPage /> : <HomePage />
 }
 
 export default App
