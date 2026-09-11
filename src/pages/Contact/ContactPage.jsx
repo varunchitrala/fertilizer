2f3f589d61b4d5e9a0d4cd0eb5c6258af21b8407
chitrala varun <varunchitralaa@gmail.com>
2026-09-11 10:21:20 +0530

    Add contact page wrapper

---
+++ src/pages/Contact/ContactPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Contact from "./Contact";
+
+function ContactPage() {
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default ContactPage;
