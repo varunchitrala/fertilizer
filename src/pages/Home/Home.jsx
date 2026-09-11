27ba998f6e0ff654e1b6ca2871d8203a1cd0101b
chitrala varun <varunchitralaa@gmail.com>
2026-09-11 12:36:54 +0530

    Integrate crop applications into Home marketing flow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import AboutPreview from "./components/AboutPreview/AboutPreview";
 import Solutions from "./components/Solutions/Solutions";
 import WhyChoose from "./components/WhyChoose/WhyChoose";
+import CropApplications from "./components/CropApplications/CropApplications";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "../../components/Footer/Footer";
 import "./home-products-preview.css";
@@ -16,6 +17,7 @@ function Home() {
         <AboutPreview />
         <Solutions />
         <WhyChoose />
+        <CropApplications />
         <section className="home-products-preview" id="products" aria-labelledby="home-products-title">
           <div className="header-container">
             <div className="home-products-preview-heading">
