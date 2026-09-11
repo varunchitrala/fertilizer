a1cdc05c82e2aff6ef3795aeab4d9a1ef20fdd27
chitrala varun <varunchitralaa@gmail.com>
2026-09-11 12:12:48 +0530

    feat: add about introduction to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../../components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import AboutPreview from "./components/AboutPreview/AboutPreview";
 import Solutions from "./components/Solutions/Solutions";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "../../components/Footer/Footer";
@@ -11,6 +12,7 @@ function Home() {
       <Header />
       <main>
         <Hero />
+        <AboutPreview />
         <Solutions />
         <section className="home-products-preview" id="products" aria-labelledby="home-products-title">
           <div className="header-container">
