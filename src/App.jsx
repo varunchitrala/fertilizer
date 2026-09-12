ee7bf6f32d2b52c36936770307b103d86f045605
chitrala varun <varunchitralaa@gmail.com>
2026-09-12 11:23:29 +0530

    Add product detail route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Products from "./pages/Products/Products";
+import ProductDetail from "./pages/Products/components/ProductDetail/ProductDetail";
 import GalleryPage from "./pages/Gallery/GalleryPage";
 import Contact from "./pages/Contact/Contact";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/gallery" element={<GalleryPage />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
